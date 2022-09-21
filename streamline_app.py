94fe3b701b5fa95e9cce2180c7f284a493e042b5
sunitaptl <113895191+sunitaptl@users.noreply.github.com>
2022-09-21 13:32:40 +0530

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,5 +1,5 @@
 
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu')streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & blueberry oatmeal')
